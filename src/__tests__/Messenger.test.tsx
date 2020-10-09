464cdfb02b0c044b4dc336838f889c5922397bbf
suprim12 <suprimofficial@yahoo.com>
2020-10-09 21:48:53 +0545

    Setup Npm

--- src/__tests__/Messenger.test.tsx
+++ src/__tests__/Messenger.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { render } from '@testing-library/react'
-import App from '../App'
+// import { render } from '@testing-library/react'
+// import App from '../App'
 
 test('Renders Messanger Without Crashing', () => {
   const div = document.createElement('div')
